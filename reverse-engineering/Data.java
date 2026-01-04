9abd01652ef859153a9b4f3673cd76081478a869
Biruk1997 <bruk307@gmail.com>
2026-01-04 04:59:28 -0800

    reverde enginering code

---
+++ reverse-engineering/Data.java
@@ -0,0 +1,22 @@
+
+
+/**
+ * @author Burak
+ * @version 1.0
+ * @created 31-Dec-2025 10:51:16 PM
+ */
+public class Data {
+
+	public Data(){
+
+	}
+
+	/**
+	 * 
+	 * @exception Throwable
+	 */
+	public void finalize()
+	  throws Throwable{
+
+	}
+}//end Data
